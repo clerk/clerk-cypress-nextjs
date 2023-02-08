99a904321110c8b9fc3ff2fd0b637fbbca378669
Dimitris <dimitris.klouvas@gmail.com>
2023-02-08 17:17:14 +0200

    Update cypress@12.5.1

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
